a74b2af6a8e396eb8aafc9704051585a72d67dfa
giulio <giulio.pilotto@stellantis.com>
2022-05-10 16:33:25 +0200

    Custom Writer

--- HTTP interface/main.go
+++ HTTP interface/main.go
@@ -7,15 +7,23 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
 	// bs := make([]byte, 99999)
 	// resp.Body.Read(bs)
 	// fmt.Println(string(bs))
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just worte this many bites:", len(bs))
+	return len(bs), nil
 }
